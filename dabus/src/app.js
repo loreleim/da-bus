7ade2470f5e359b58d0c2bc3ba35c474095a0cf2
loreleim <lorelei.miyamura@gmail.com>
2019-09-29 21:50:00 -0500

    Initial React initialization

---
+++ dabus/src/app.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./app.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>daBus Hawaii</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
