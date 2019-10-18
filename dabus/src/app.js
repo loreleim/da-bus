5c8e69409230a92d4b0f3c2f77f92e5f544fe34d
Lorelei <lorelei.miyamura@gmail.com>
2019-10-18 10:07:01 -0500

    Loading in the React Router dependency

--- dabus/src/app.js
+++ dabus/src/app.js
@@ -2,6 +2,7 @@ import React from "react";
 import "./style/index.scss";
 //import Footer from "./components/footer-template/index.js";
 import style from "./style/index.scss";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Nav from "./components/nav/index.js";
 import Home from "./pages/home/index.js";
 
