6e8c44c2e1f3c4448ece0628f8f3071ab3b86859
Lorelei <lorelei.miyamura@gmail.com>
2019-10-18 10:35:47 -0500

    Creating the about page & styling

---
+++ dabus/src/pages/about/index.js
@@ -0,0 +1,15 @@
+import React from "react";
+//import { observer, inject } from "mobx-react";
+
+class About extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>About</h1>
+      </div>
+    );
+  }
+}
+
+export default About;
+//export default inject("appData")(observer(Card));
