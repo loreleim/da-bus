8d2df1bc8270908b6767ade1c57658350ae1d87b
Lorelei <lorelei.miyamura@gmail.com>
2019-10-17 20:38:05 -0500

    Pulling the home page to render first as a component

--- dabus/src/app.js
+++ dabus/src/app.js
@@ -2,9 +2,12 @@ import React from "react";
 import "./style/index.scss";
 //import Footer from "./components/footer-template/index.js";
 import Nav from "./components/nav/index.js";
+import Home from "./pages/home/index.js";
 
 function App() {
   return (
+    <div>
+      <Home></Home>
       <Nav></Nav>
     </div>
   );
