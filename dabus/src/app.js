b4dde5abf0d914f994656f4e53db9619bf7bb0b1
Lorelei <lorelei.miyamura@gmail.com>
2019-10-18 10:06:42 -0500

    importing the stylesheet as a stylsheet to use style.

--- dabus/src/app.js
+++ dabus/src/app.js
@@ -1,6 +1,7 @@
 import React from "react";
 import "./style/index.scss";
 //import Footer from "./components/footer-template/index.js";
+import style from "./style/index.scss";
 import Nav from "./components/nav/index.js";
 import Home from "./pages/home/index.js";
 
