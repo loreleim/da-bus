24ea71d61ba626b1491b0e11e2b15974d5fb8cf8
Lorelei <lorelei.miyamura@gmail.com>
2019-10-17 20:38:19 -0500

    Initial setup for the corner logo component

---
+++ dabus/src/components/corner-logo/index.js
@@ -0,0 +1,9 @@
+import React, { Component } from "react";
+import { ReactComponent as LogoSVG } from "../../images/";
+import style from "./index.module.scss";
+
+export default class CornerLogo extends Component {
+  render() {
+    return <div className={style.container}></div>;
+  }
+}
