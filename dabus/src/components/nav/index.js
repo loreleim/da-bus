3078a28c7c16ace2a837bd53c9bf528a3a663d44
Lorelei <lorelei.miyamura@gmail.com>
2019-10-18 10:47:26 -0500

    Adding the Router Links to the Nav Bar

--- dabus/src/components/nav/index.js
+++ dabus/src/components/nav/index.js
@@ -1,16 +1,27 @@
 import React, { Component } from "react";
+import { Link } from "react-router-dom";
 import style from "./index.module.scss";
 
 export default class Nav extends Component {
   render() {
     return (
       <div className={style.container}>
-        <ul>
-          <li>Reload Card</li>
-          <li>Trip Planner</li>
-          <li>About</li>
-          <li>Sign In</li>
-        </ul>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/fares">Fares</Link>
+            </li>
+            <li>
+              <Link to="/tripplanner">Trip Planner</Link>
+            </li>
+            <li>
+              <Link to="/about">About</Link>
+            </li>
+            <li>
+              <Link to="/login">Login</Link>
+            </li>
+          </ul>
+        </nav>
       </div>
     );
   }
