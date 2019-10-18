a5dbe9beb9f052bf8bc7ea57d33dccaebfbaea36
Lorelei <lorelei.miyamura@gmail.com>
2019-10-18 10:46:40 -0500

    Adding home route redirect to the corner logo

--- dabus/src/components/corner-logo/index.js
+++ dabus/src/components/corner-logo/index.js
@@ -1,14 +1,15 @@
 import React, { Component } from "react";
 import { ReactComponent as LogoSVG } from "../../images/dabus.svg";
+import { Link } from "react-router-dom";
 import style from "./index.module.scss";
 
 export default class CornerLogo extends Component {
   render() {
     return (
       <div className={style.container}>
-        <a href="https://google.com">
+        <Link to="/">
           <LogoSVG></LogoSVG>
-        </a>
+        </Link>
       </div>
     );
   }
