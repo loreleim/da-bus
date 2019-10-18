b70b30a1d62abd89a2f7ca97c6c24f11b5a9090a
Lorelei <lorelei.miyamura@gmail.com>
2019-10-17 20:42:48 -0500

    Adding the menu names on the nav bar

--- dabus/src/components/nav/index.js
+++ dabus/src/components/nav/index.js
@@ -4,8 +4,13 @@ import style from "./index.module.scss";
 export default class Nav extends Component {
   render() {
     return (
-      <div {...this.props}>
-        <div className={style.container}>Navigation</div>
+      <div className={style.container}>
+        <ul>
+          <li>Reload Card</li>
+          <li>Trip Planner</li>
+          <li>About</li>
+          <li>Sign In</li>
+        </ul>
       </div>
     );
   }
