40ffee74399e69f7414c2ff084d865d9e96aedc2
Lorelei <lorelei.miyamura@gmail.com>
2019-10-18 10:50:44 -0500

    Deleting unused styling

--- dabus/src/app.js
+++ dabus/src/app.js
@@ -1,6 +1,4 @@
 import React from "react";
-import "./style/index.scss";
-//import Footer from "./components/footer-template/index.js";
 import style from "./style/index.scss";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Nav from "./components/nav/index.js";
