71315bc6b432123af4a81854de5bd196ba1e65b4
Lorelei <lorelei.miyamura@gmail.com>
2019-10-18 10:52:06 -0500

    importing the About page

--- dabus/src/app.js
+++ dabus/src/app.js
@@ -5,6 +5,7 @@ import Nav from "./components/nav/index.js";
 import Home from "./pages/home/index.js";
 import Fares from "./pages/fares/index.js";
 import TripPlanner from "./pages/trip-planner/index.js";
+import About from "./pages/about/index.js";
 
 function App() {
   return (
