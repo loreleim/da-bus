fa537fab1e95bf5861e1fbc932f2a4040d45189f
Lorelei <lorelei.miyamura@gmail.com>
2019-10-24 19:29:09 -0500

    Putting in a main container

--- dabus/src/pages/home/index.js
+++ dabus/src/pages/home/index.js
@@ -6,23 +6,27 @@ import CornerLogo from "../../components/corner-logo";
 class Home extends React.Component {
   render() {
     return (
-      <div className={style.container}>
-        <CornerLogo></CornerLogo>
-        <div className={style.searchContainer}>
-          <h1>Where you like go?</h1>
-          <form>
-            <div>
-              <input className={style.searchInput}></input>
-              <input className={style.searchInput}></input>
-            </div>
-            <div>
-              <input
-                type="button"
-                value="We Go"
-                className={style.button}
-              ></input>
-            </div>
-          </form>
+      <div className={style.mainContainer}>
+        <div className={style.littleDot}></div>
+        <div className={style.bigDot}></div>
+        <div className={style.container}>
+          <CornerLogo></CornerLogo>
+          <div className={style.searchContainer}>
+            <h1>Where you like go?</h1>
+            <form>
+              <div>
+                <input className={style.searchInput}></input>
+                <input className={style.searchInput}></input>
+              </div>
+              <div>
+                <input
+                  type="button"
+                  value="We Go"
+                  className={style.button}
+                ></input>
+              </div>
+            </form>
+          </div>
         </div>
       </div>
     );
