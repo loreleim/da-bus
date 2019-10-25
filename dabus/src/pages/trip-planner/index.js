0c9e7599a57925c68ccb921e3d6e1c218db8364c
Lorelei <lorelei.miyamura@gmail.com>
2019-10-24 20:01:34 -0500

    Setup for the trip planner

--- dabus/src/pages/trip-planner/index.js
+++ dabus/src/pages/trip-planner/index.js
@@ -1,9 +1,20 @@
 import React from "react";
+import style from "./index.module.scss";
+import CornerLogo from "../../components/corner-logo";
+
 class TripPlanner extends React.Component {
   render() {
     return (
       <div>
-        <h1>Login</h1>
+        <div className={style.container}>
+          <CornerLogo></CornerLogo>
+          <div className={style.routeContainer}>
+            <div className={style.nameContainer}>
+              <h2>Lorelei's Saved Routes</h2>
+            </div>
+            <hr></hr>
+          </div>
+        </div>
       </div>
     );
   }
