d2daed71a0ce90aa568bbd002bb2bbfce2c17d1a
Lorelei <lorelei.miyamura@gmail.com>
2019-10-24 20:13:57 -0500

    Updates

---
+++ precache-manifest.c16f1fbec91a6f22a5cbf248e8ee3498.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "660ec8c442bb0004dc177eb9d3a44b8b",
+    "url": "/da-bus/index.html"
+  },
+  {
+    "revision": "561273c1e5f6532f7c2d",
+    "url": "/da-bus/static/css/main.aa939aae.chunk.css"
+  },
+  {
+    "revision": "b70e3d4ebf525b0547a9",
+    "url": "/da-bus/static/js/2.3e9fe96a.chunk.js"
+  },
+  {
+    "revision": "561273c1e5f6532f7c2d",
+    "url": "/da-bus/static/js/main.6548808d.chunk.js"
+  },
+  {
+    "revision": "b110d4dd916c720ee9f2",
+    "url": "/da-bus/static/js/runtime-main.0a1a1cf6.js"
+  },
+  {
+    "revision": "147d0449315efd3d67402eb8534e63ad",
+    "url": "/da-bus/static/media/dabus.147d0449.svg"
+  },
+  {
+    "revision": "bfc485e058f8b5b7b5b571d0b3ed3bec",
+    "url": "/da-bus/static/media/home-map.bfc485e0.png"
+  }
+]);
