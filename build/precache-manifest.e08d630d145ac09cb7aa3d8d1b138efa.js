22fadffd33ce4d1e1b209a4ea7c20e6a5f9162ba
Lorelei <lorelei.miyamura@gmail.com>
2019-10-24 20:29:58 -0500

    Moving it out of the non-root folder

---
+++ build/precache-manifest.e08d630d145ac09cb7aa3d8d1b138efa.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3cb16aa49c7413fd484f58bea94a1e35",
+    "url": "/da-bus/index.html"
+  },
+  {
+    "revision": "36ae274b6ce1fe0dbf4f",
+    "url": "/da-bus/static/css/main.aa939aae.chunk.css"
+  },
+  {
+    "revision": "b70e3d4ebf525b0547a9",
+    "url": "/da-bus/static/js/2.3e9fe96a.chunk.js"
+  },
+  {
+    "revision": "36ae274b6ce1fe0dbf4f",
+    "url": "/da-bus/static/js/main.9783ce53.chunk.js"
+  },
+  {
+    "revision": "b110d4dd916c720ee9f2",
+    "url": "/da-bus/static/js/runtime-main.0a1a1cf6.js"
+  },
+  {
+    "revision": "147d0449315efd3d67402eb8534e63ad",
+    "url": "/da-bus/static/media/dabus.147d0449.svg"
+  },
+  {
+    "revision": "bfc485e058f8b5b7b5b571d0b3ed3bec",
+    "url": "/da-bus/static/media/home-map.bfc485e0.png"
+  }
+]);
